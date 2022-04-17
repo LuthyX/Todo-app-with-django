88f2fc74c696c5f5a115b5d43c48a8e0782e2052
LuthyX <peteradegoke81@gmail.com>
2022-04-17 09:34:08 +0100

    all

---
+++ todo/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect, render
+from .models import Todo
+
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST['title'])
+        new_todo.save()
+    return render(request, 'index.html', {'todos':todos})
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect ('/')
